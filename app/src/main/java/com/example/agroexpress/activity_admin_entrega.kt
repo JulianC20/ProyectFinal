a1bff07d42c99d75dc8e410ba59b6b6b80637bee
H01man <holmanandresrodriguezleon@gmail.com>
2023-05-03 07:46:59 -0500

    Recycler's views faltantes Holman

---
+++ app/src/main/java/com/example/agroexpress/activity_admin_entrega.kt
@@ -0,0 +1,11 @@
+package com.example.agroexpress
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_admin_entrega : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_entrega)
+    }
+}
