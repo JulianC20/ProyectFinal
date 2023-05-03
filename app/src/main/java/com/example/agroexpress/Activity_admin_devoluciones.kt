0b6db10853d7ca160254038ef72fa5be6b31ba06
Julain S <Salazardix321@gmail.com>
2023-05-03 17:20:58 -0500

    Recyclers_Final

---
+++ app/src/main/java/com/example/agroexpress/Activity_admin_devoluciones.kt
@@ -0,0 +1,11 @@
+package com.example.agroexpress
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_admin_devoluciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_devoluciones)
+    }
+}
