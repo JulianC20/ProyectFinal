f9275de2c154329ff97a8df9d4775be564653b6a
Santiago1034 <107003145+Santiago1034@users.noreply.github.com>
2023-05-05 11:34:52 -0500

    carrusel

--- app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
+++ app/src/main/java/com/example/agroexpress/Activity_Inicio_Roles.kt
@@ -13,12 +13,16 @@ import org.json.JSONObject
 
 class Activity_Inicio_Roles : AppCompatActivity() {
 
+    private val list = mutableListOf<CarouselItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_roles)
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+        list.add(CarouselItem("https://static.vecteezy.com/system/resources/previews/002/266/395/original/mixed-fruits-with-apple-banana-orange-and-other-free-photo.jpg"))
+        list.add(CarouselItem("https://static.vecteezy.com/system/resources/previews/002/266/395/original/mixed-fruits-with-apple-banana-orange-and-other-free-photo.jpg"))
+        list.add(CarouselItem("https://cdn-bbolm.nitrocdn.com/rhVOyqGRzkOemInDdraPQRtaBJmVBfaM/assets/images/optimized/rev-8e07b84/wp-content/uploads/2023/03/30-nombres-de-frutas-en-ingles-y-sus-significados-1080x675.jpg"))
+        carousel.addData(list)
+        }
     }
-   /* fun buscar(view: View) {
-        val intent = Intent(this, AdminCampesinosActivity::class.java).apply { }
-        startActivity(intent)
-    }*/
-}
+
--- app/src/main/java/com/example/agroexpress/AdmiProductosFragment.kt
+++ app/src/main/java/com/example/agroexpress/AdmiProductosFragment.kt
@@ -1,10 +1,13 @@
 package com.example.agroexpress
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,24 +39,14 @@ class AdmiProductosFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admi_productos, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bottomCamp: Button = view.findViewById(R.id.btnEditProduc)
+        bottomCamp.setOnClickListener {
+            val intent = Intent(getActivity(), edit_Product_Activity::class.java)
+            startActivity(intent)
+        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AdmiProductosFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AdmiProductosFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
     }
 }
